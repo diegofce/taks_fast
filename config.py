555ec9714ac7463fa28cd31c6df79dfac2d3e5a9
DiegoFChacon <chacondiego31@gmail.com>
2026-03-02 16:50:43 -0500

    Add initial project structure with configuration, database setup, and requirements

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from database.base import Base
+from database.session import get_db
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123456789")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5433")
+DB_NAME = os.getenv("DB_NAME", "tareas_db")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+class Settings:
+    DB_USER = DB_USER
+    DB_PASSWORD = DB_PASSWORD
+    DB_HOST = DB_HOST
+    DB_PORT = DB_PORT
+    DB_NAME = DB_NAME
+    DATABASE_URL = DATABASE_URL
+
+settings = Settings()
